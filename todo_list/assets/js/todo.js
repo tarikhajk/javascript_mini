c27bfc4f37f36e59f3b332b5e70d6a54a58a53ea
Tarik <tarikh@uw.edu>
2019-01-04 16:05:55 -0800

    Completed Todo list app

--- todo_list/assets/js/todo.js
+++ todo_list/assets/js/todo.js
@@ -1,4 +1,25 @@
 // Check off specific "li"s by striking through them when clicked
-$("li").on("click", function() {
+$("ul").on("click", "li", function () {
   $(this).toggleClass("todo_done");
 });
+
+// Add "li" when enter is pressed
+$("input").on("keypress", function (event) {
+  if (event.which === 13) {
+    var todo = $(this).val();
+    $("ul").append("<li><span><i class=\"fa fa-trash\" aria-hidden=\"true\"></i> </span>" + todo + "</li>");
+    $(this).val("")
+  }
+})
+
+$("h1 i").on("click",function() {
+  $("input").fadeToggle();
+})
+
+// Click on X to fade out then delete todo
+$("ul").on("click", "span", function (event) {
+  $(this).parent().fadeOut(function () {
+    $(this).remove();
+  });
+  event.stopPropagation();
+})
